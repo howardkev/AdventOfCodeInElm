fca3e3813831424c679e92de4c82321576f05d58
kevin <howard.kev@gmail.com>
2022-12-19 19:29:57 -0800

    getting there

--- 2022/src/Day19.elm
+++ 2022/src/Day19.elm
@@ -24,168 +24,155 @@ sampleInput =
 Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
 """
+
+type alias Key = (Int, List Int, List Int)
+type alias Cache = Dict Key Int
+
 type alias Blueprint =
     { id: Int
     , ore: Int
     , clay: Int
     , obsidian: (Int, Int)
     , geode: (Int,Int)
+    , maxSpend: List Int
+    , recipes: List (List (Int, Int))
     }
 
 part1 input =
     input
         |> parseInput
         |> take 1
-        |> map solve
+        |> List.map solve
 
 parseInput input =
     input
         |> String.split "\n"
-        |> map ints
-        |> filterMap toBlueprints
-
+        |> List.map ints
+        |> List.filterMap toBlueprints
+        
 toBlueprints : List Int -> Maybe Blueprint
 toBlueprints line =
     case line of
         [a,b,c,d,e,f,g] ->
-            Just { id = a, ore = b, clay = c, obsidian = (d,e), geode = (f,g)}
+            Just { id = a, ore = b, clay = c, obsidian = (d,e), geode = (f,g)
+                , maxSpend = [ max (max b c) (max d f), e, g]
+                , recipes = [[(b, 0)], [(c, 0)], [(d, 0), (e, 1)], [(f, 0), (g, 2)]]
+                }
         _ -> Nothing
 
 solve blueprint =
     let
-        _ = D.log "running blueprint" blueprint.id
         cache = Dict.empty
-        minutes = 25
+        minutes = 24
         robots = [1, 0, 0, 0]
         minerals = [0, 0, 0, 0]
     in
     dfs blueprint cache minutes robots minerals
-        |> Tuple.second
     
-type alias Key = (Int, List Int, List Int)
-type alias Cache = Dict Key Int
-
 dfs : Blueprint -> Cache -> Int -> List Int -> List Int -> (Cache, Int)
 dfs bp cache minutes robots minerals =
     let
         key = (minutes, robots, minerals)
-        getMineralAt index list = LE.getAt index list |> Maybe.withDefault 0
+        getAt i list = LE.getAt i list |> Maybe.withDefault 0
     in
-    if minutes == 0 then
-        (cache, getMineralAt 3 minerals)
+    if minutes <= 20 then
+        --let
+        --    _ = Debug.log "reject" (minutes, robots, minerals)
+        --in
+        (cache, getAt 3 minerals)
     else
         case Dict.get key cache of
             Just n -> (cache, n)
             Nothing ->
                 let
-                    newMinerals = List.map2 (\mineral bot ->
-                            mineral + bot
-                        ) minerals robots
-
-                    buyNothing = Just (newMinerals, robots)
-
-                    createOre = 
-                        if getMineralAt 0 minerals >= bp.ore then
-                            Just 
-                                ( List.indexedMap (\i mineral ->
-                                        if i == 0 then
-                                            mineral - bp.ore
-                                        else
-                                            mineral
-                                    ) newMinerals
-                                , List.indexedMap (\i bot ->
-                                        if i == 0 then
-                                            bot + 1
-                                        else
-                                            bot
-                                    ) robots
+                    --_ = Debug.log "here" (minutes, robots, minerals)
+                    maxVal = (getAt 3 minerals) + (getAt 3 robots) * minutes
+                        
+                    toCheck = List.indexedMap (\robot recipe ->
+                            if robot /= 3 && getAt robot robots >= getAt robot bp.maxSpend then
+                                Nothing
+                            else
+                                Just (robot, recipe)
+                        ) bp.recipes
+                        |> List.filterMap identity
+                        
+                    hasNothing list =
+                        List.any (\a -> 
+                                case a of
+                                    Nothing -> True
+                                    _ -> False
+                            ) list
+                            
+                    waitCalculation list =
+                        list
+                            |> List.map (\(amount, rtype) ->
+                                    if getAt rtype robots == 0 then
+                                        Nothing
+                                    else
+                                        Just ((amount - (getAt rtype minerals)) // getAt rtype robots)
                                 )
-                        else
-                            Nothing
-
-                    createClay = 
-                        if getMineralAt 0 minerals >= bp.clay then
-                            Just 
-                                ( List.indexedMap (\i mineral ->
-                                        if i == 0 then
-                                            mineral - bp.clay
-                                        else
-                                            mineral
-                                    ) newMinerals
-                                , List.indexedMap (\i bot ->
-                                        if i == 1 then
-                                            bot + 1
-                                        else
-                                            bot
-                                    ) robots
-                                )
-                        else
-                            Nothing
-
-                    createObsidian = 
+                        
+                    waits = toCheck
+                        |> List.map (\(a, b) -> 
+                                (a, waitCalculation b)
+                            )
+                        |> List.filterMap (\(a, b) ->
+                                if hasNothing b then
+                                    Nothing
+                                else
+                                    Just (a, List.filterMap identity b)
+                            )
+                        |> List.map (\(a, b) -> 
+                                (a, List.maximum b |> Maybe.withDefault 0)
+                            )
+                        
+                    remaining = List.map (\(a, wait) ->
+                            (a, minutes - wait - 1, wait)
+                        ) waits
+                        |> List.filter (\(_, b, _) -> b > 0)
+                        
+                    calculateMineralsH (bot,remain,wait) = 
                         let
-                            (ore, clay) = bp.obsidian
+                            --_ = Debug.log "zip" (bot,remain,wait)
+                            zip = LE.zip minerals robots
+                            recipe = LE.getAt bot bp.recipes |> Maybe.withDefault [(0,0)]
+                            amounts = List.map (\(x, y) -> x + y * (wait + 1)) zip
+                            adjusted = List.foldl (\(ramt, rtype) (amts, bots) ->
+                                    let
+                                        --_ = Debug.log "bots" (bots, bot, newBots)
+                                        newBots = List.indexedMap (\i b ->
+                                                if i == bot then
+                                                    b + 1
+                                                else
+                                                    b
+                                            ) bots
+                                        currentAmt = LE.getAt rtype amts |> Maybe.withDefault 0
+                                        newAmounts = 
+                                            LE.setAt rtype (min (currentAmt - ramt) (
+                                                    remain * (getAt rtype bp.maxSpend)
+                                                )) amts
+                                    in
+                                    ( newAmounts, newBots)
+                                ) (amounts, robots) recipe
                         in
-                        if (getMineralAt 0 minerals >= ore &&
-                            getMineralAt 1 minerals >= clay) then
-                            Just 
-                                ( List.indexedMap (\i mineral ->
-                                        if i == 0 then
-                                            mineral - ore
-                                        else if i == 1 then
-                                            mineral - clay
-                                        else
-                                            mineral
-                                    ) newMinerals
-                                , List.indexedMap (\i bot ->
-                                        if i == 2 then
-                                            bot + 1
-                                        else
-                                            bot
-                                    ) robots
-                                )
-                        else
-                            Nothing
-
-                    createGeode = 
-                        let
-                            (ore, obsidian) = bp.geode
-                        in
-                        if (getMineralAt 0 minerals >= ore &&
-                            getMineralAt 2 minerals >= obsidian) then
-                            Just 
-                                ( List.indexedMap (\i mineral ->
-                                        if i == 0 then
-                                            mineral - ore
-                                        else if i == 2 then
-                                            mineral - obsidian
-                                        else
-                                            mineral
-                                    ) newMinerals
-                                , List.indexedMap (\i bot ->
-                                        if i == 3 then
-                                            bot + 1
-                                        else
-                                            bot
-                                    ) robots
-                                )
-                        else
-                            Nothing
-
-                    nextStates = filterMap identity [buyNothing, createOre, createClay, createObsidian, createGeode]
-
-                    (c2, geodesList) = foldl (\(m, bots) (csh, lst) -> 
+                        ((bot,remain,wait), adjusted)
+                        
+                    nextRuns = remaining
+                        |> List.map calculateMineralsH
+                        
+                    (c2, geodesList) = List.foldl (\((bot, remain, wait), (amounts, bots)) (csh, lst) ->
                             let
-                                (c1, value) = dfs bp csh (minutes - 1) bots m
+                                --_ = Debug.log "inside" (minutes, (remain, bots, amounts))
+                                (c1, value) = dfs bp csh remain bots amounts
                             in
                             (c1, value::lst)
-                        ) (cache, []) nextStates
-                        --|> D.log "results"
-
-                    bestGeode = maximum geodesList
+                        ) (cache, []) nextRuns
+                    
+                    bestGeode = maximum (maxVal::geodesList)
                         |> Maybe.withDefault 0
 
                     c3 = Dict.insert key bestGeode c2
+                    
                 in
                 (c3, bestGeode)
 
